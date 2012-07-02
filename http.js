7407aa52816eb62d2162de51342119ee024fabf1
xonecas <xonecas@edshelf.com>
2012-07-02 12:18:02 -0700

    added new colorscheme

---
+++ http.js
@@ -0,0 +1,15 @@
+/*jslint node:true nomen:true */
+var connect, server, root, colors;
+root = __dirname;
+connect = require('connect');
+colors = require('colors');
+server = connect()
+    .use(connect.favicon('favicon.ico'))
+    .use(connect.logger('dev'))
+    .use(connect.compress())
+    //.use(connect['static'](root, { maxAge: 31536000000 })) // one year.
+    .use(connect['static'](root)) // one year.
+    .use(connect.errorHandler())
+    .listen(process.argv[2] ? 80 : 8080);
+
+console.log('Server is up...'.blue, root.red);
